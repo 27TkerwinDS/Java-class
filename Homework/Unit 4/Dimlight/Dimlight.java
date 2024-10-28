2d2edb688561cf790591f83d6b1c237e29d3a59b
27TkerwinDS <147544921+27TkerwinDS@users.noreply.github.com>
2024-10-28 12:42:41 -0400

    Update Dimlight.java

--- Homework/Unit 4/Dimlight/Dimlight.java
+++ Homework/Unit 4/Dimlight/Dimlight.java
@@ -10,6 +10,9 @@ class Light {
 
     // this method returns true if the light is on.
     public int view() {
+        if (lightlevel!=0){
+            lightlevel=lightlevelon;
+        }
         return lightlevel;
     }
 
@@ -23,8 +26,21 @@ class Light {
         lightlevel=0;
     }
     public void setLight(int newval){
-        lightlevelon=newval;
+        if(newval<=10&&newval>=1){
+            lightlevelon=newval;
         lightlevel=lightlevelon;
+        }else{
+            System.err.println("1-10 please!");
+        } 
+    }
+    public void up() {
+        if(lightlevelon<10){
+            lightlevelon=lightlevelon+1;
+        }
+    }public void down() {
+        if(lightlevelon>1){
+            lightlevelon=lightlevelon-1;
+        }
     }
 }
 
@@ -32,17 +48,29 @@ public class Dimlight {
     public static void main(String[] args) {
         Scanner in=new Scanner (System.in);
         boolean q=false;
+        Light light=new Light();
         while(q==false){
-            System.out.printf("Please enter 'on' to turn on the light%nPlease enter 'off' to turn off the light%nplease enter 'veiw' to see the light value%nplease enter 'up' to increase brightness of the light%nplease enter 'down' to turn down the brightness of the light%nPlease enter 1-10 to set the brightness of the light%nEnter 'q' to quit.%n");
-            if(in.hasNextLine()){
+            System.out.printf("%nPlease enter 'on' to turn on the light%nPlease enter 'off' to turn off the light%nplease enter 'view' to see the light value%nplease enter 'up' to increase brightness of the light%nplease enter 'down' to turn down the brightness of the light%nPlease enter 1-10 to set the brightness of the light%nEnter 'q' to quit.%n");
+            if(in.hasNextInt()){
+                int input=in.nextInt();
+                light.setLight(input);
+            }else if(in.hasNextLine()){
                 String input=in.nextLine();
                 if(input.equals("q")){
                     q=true;
                 }else if(input.equals("on")){
-                    Light.turnOn();
+                    light.turnOn();
+                }else if(input.equals("off")){
+                    light.turnOff();
+                }else if(input.equals("view")){
+                    System.out.println(light.view());
+                }else if(input.equals("up")){
+                    light.up();
+                }else if(input.equals("down")){
+                    light.down();
+                }else{
+                    System.err.println("That is not an acseptible input!");
                 }
-            }else if (in.hasNextInt()){
-
             }
         }
         in.close();
