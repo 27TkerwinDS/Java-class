4cb836e5666788b9bea6b0db2c492d0b8f125256
27TkerwinDS <147544921+27TkerwinDS@users.noreply.github.com>
2024-11-18 12:31:21 -0500

    hi

--- Homework/Unit 5/lesson 7/PartA.java
+++ Homework/Unit 5/lesson 7/PartA.java
@@ -7,15 +7,21 @@ Translate (dx,dy) - translate the point in the x-direction (dx) and in the y dir
 Distance to (point) - returns the distance to another point; takes the other point as an argument.
 */ 
 class Cords{
-    double x=0;
-    double y=0;
-    double theta=0;
-    double r=0;
+    double x;
+    double y;
+    double theta;
+    double r;
     public void setPoint(double newx, double newy){
         x=newx;
         y=newy;
         r=Math.sqrt(x*x+r*r);
         theta=Math.toDegrees(Math.atan((y)/(x)));
+    }
+    public Cords(){
+        x=0;
+        y=0;
+        theta=0;
+        r=0;
     }
     public double returnX(){
         return x;
@@ -42,6 +48,18 @@ class Cords{
     public void translateY(double trans){
         y=y+trans;
         r=Math.sqrt(x*x+r*r);
+        theta=Math.toDegrees(Math.atan2((y),(x)));
+    }
+    public double distance(Cords c){
+
+        double distance=Math.sqrt((x-c.returnX())*(x-c.returnX())+(y-c.returnY())*(y-c.returnY()));
+
+        return distance;
+    }
+    public Cords(double newx, double newy){
+        x=newx;
+        y=newy;
+        r=Math.sqrt(x*x+r*r);
         theta=Math.toDegrees(Math.atan((y)/(x)));
     }
 
@@ -49,6 +67,7 @@ class Cords{
 public class PartA{
     public static void main(String[] args) {
         Cords c=new Cords();
+        Cords d=new Cords(2,2);
         c.setPoint(3,1);
         System.out.println("X="+c.returnX());
         System.out.println("Y="+c.returnY());
@@ -57,5 +76,7 @@ public class PartA{
         System.out.println("theta="+c.returnTheta());
         c.translateX(1);
         System.out.println("new all="+c.returnAll());
+        c.setPoint(1, 1);
+        System.out.println(d.distance(c));
     }
 }
