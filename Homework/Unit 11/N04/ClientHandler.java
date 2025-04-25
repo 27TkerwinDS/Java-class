f3ae18654bb35d46be77018219988c6dca35a025
27TkerwinDS <147544921+27TkerwinDS@users.noreply.github.com>
2025-04-25 07:42:18 -0400

    Update ClientHandler.java

--- Homework/Unit 11/N04/ClientHandler.java
+++ Homework/Unit 11/N04/ClientHandler.java
@@ -1,11 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
-import java.util.ArrayList;
 
 public class ClientHandler implements Runnable{
     private Socket clientSocket;
     private String inputLine;
-    PrintWriter out;
+    static PrintWriter out;
     public static void sendMessage(String message){
         if (out !=null){
             out.println(message);
@@ -22,7 +24,19 @@ public class ClientHandler implements Runnable{
         // use try with catch statement to create output and input streams.
         
         try{
-            
+            out=new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            System.out.println("Connected to client " + clientSocket.getInetAddress().getHostAddress()+ " on port " + clientSocket.getPort());
+            while ((inputLine = in.readLine()) != null) {
+                // display info about what was received.
+                System.out.printf("Received from client (%s): %s%n",
+                        clientSocket.getInetAddress().getHostAddress(), inputLine);
+                // echo line back to client.
+                out.println(inputLine);
+            }
+            System.out.println("Client disconnected: " + clientSocket.getInetAddress());
+        }catch (IOException e) {
+            System.err.println("Error handling client: " + e.getMessage());
         }
     }
 }
