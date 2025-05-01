2f014ab24af7f0ffab0ffe754923d869ac0c516a
27TkerwinDS <147544921+27TkerwinDS@users.noreply.github.com>
2025-05-01 11:42:55 -0400

    hi

--- Homework/Unit 11/N04/EchoCliant.java
+++ Homework/Unit 11/N04/EchoCliant.java
@@ -5,7 +5,7 @@ import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
-public class EchoCliant {
+public class EchoCliant implements Runnable{
     public static void main(String[] args) {
         if (args.length != 2) {
             System.err.println(
@@ -26,14 +26,15 @@ public class EchoCliant {
         try (
                Socket echoSocket = new Socket(hostName, portNumber);
                PrintWriter out = new PrintWriter(echoSocket.getOutputStream(), true);
-               BufferedReader in = new BufferedReader(
-                       new InputStreamReader(echoSocket.getInputStream()));
                BufferedReader stdIn = new BufferedReader(
                        new InputStreamReader(System.in))) {
+            //create message handler
+            MessageHandler handler=new MessageHandler(echoSocket);
+            Thread thread=new Thread(handler);
+            thread.start();
            String userInput;
            while ((userInput = stdIn.readLine()) != null) {
                out.println(userInput);
-               System.out.println("echo: " + in.readLine());
            }
        } catch (UnknownHostException e) {
            System.err.println("Don't know about host " + hostName);
@@ -44,5 +45,11 @@ public class EchoCliant {
            System.exit(1);
        }
 
+    }
+
+    @Override
+    public void run() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'run'");
     }
 }
